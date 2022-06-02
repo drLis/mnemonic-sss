640788cc7e5dcbcf77050a701005ad5efd2af499
dr.lis <dr.lis@outlook.com>
2022-06-03 00:30:17 +1000

    Completed sss module

--- scripts/backup_mnemonic_using_sss.py
+++ scripts/backup_mnemonic_using_sss.py
@@ -11,5 +11,19 @@ def ssss_encrypt(mnemonic, minimum, number_of_shares):
 	if (len(mnemonic.split()) > 12):
 		raise Exception("Only 12 words in mnemonic for proof-of-concept")
 	entropy = mnemo.to_entropy(mnemonic)
-	shares = Shamir.split(minimum, number_of_shares, int.from_bytes(entropy, "big"))
-	return shares
+	shares = Shamir.split(minimum, number_of_shares, entropy)
+	return list(map(share_to_mnemonic, shares))
+
+def share_to_mnemonic(share):
+	share_bytes = share[1]
+	mnemonic_for_share = mnemo.to_mnemonic(share_bytes)
+	return (share[0], mnemonic_for_share)
+
+def mnemonic_to_share(mnemonic_for_share):
+	share_bytes = mnemo.to_entropy(mnemonic_for_share[1])
+	return (mnemonic_for_share[0], share_bytes)
+
+def ssss_decrypt(mnemonic_shares):
+	shares = list(map(mnemonic_to_share, mnemonic_shares))
+	entropy = Shamir.combine(shares)
+	return mnemo.to_mnemonic(entropy)
